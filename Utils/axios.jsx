b7b1bdc63fb93598339f49600db4fd642a652e21
atharva00-7 <tandaleatharva91203@gmail.com>
2024-08-06 18:49:59 +0530

    final push

--- Utils/axios.jsx
+++ Utils/axios.jsx
@@ -4,11 +4,7 @@ const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3/",
     headers: {
       accept: 'application/json',
-<<<<<<< HEAD
       Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`
-=======
-      Authorization: 'Bearer xxxxxx'
->>>>>>> 4814f98ea5e19a5dcf7505c90bea67a95ce0c157
       }
 });
 
