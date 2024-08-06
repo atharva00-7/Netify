7c0ebaf01e3a5bcd08ea5cccd771ae9aabac3182
atharva00-7 <tandaleatharva91203@gmail.com>
2024-08-06 18:43:15 +0530

    final commit

--- Utils/axios.jsx
+++ Utils/axios.jsx
@@ -4,7 +4,7 @@ const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3/",
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNmFjZjJiZDkxZTg0MDQ2YzY1YzhkMzg3Nzg4ZjAyMyIsInN1YiI6IjY1ZGIzODlkODI2MWVlMDE4NWMyY2EzZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eng2Vap2ZE90O2VXlqNsblY9ObwqVHBM1KOquqCsTkg'
+      Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`
       }
 });
 
